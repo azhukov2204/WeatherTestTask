6a47936201bc9a6788008b9fb5279a600795aecd
azhukov2204 <andrew284@mail.ri>
2021-07-27 16:07:23 +0500

    added kotlin configuration

--- app/src/main/java/ru/androidlearning/weathertesttask/ui/main/MainViewModel.java
+++
@@ -1,7 +0,0 @@
-package ru.androidlearning.weathertesttask.ui.main;
-
-import androidx.lifecycle.ViewModel;
-
-public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
---
+++ app/src/main/java/ru/androidlearning/weathertesttask/ui/main/MainViewModel.kt
@@ -0,0 +1,6 @@
+package ru.androidlearning.weathertesttask.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() { // TODO: Implement the ViewModel
+}
