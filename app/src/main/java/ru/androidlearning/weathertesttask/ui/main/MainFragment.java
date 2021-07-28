34fc1b4b051d0e3c9764b1427e1e3447e97a0711
azhukov2204 <andrew284@mail.ri>
2021-07-28 13:45:04 +0500

    get weather by coordinates

--- app/src/main/java/ru/androidlearning/weathertesttask/ui/main/MainFragment.java
+++ app/src/main/java/ru/androidlearning/weathertesttask/ui/main/MainFragment.java
@@ -1,10 +1,21 @@
 package ru.androidlearning.weathertesttask.ui.main;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -12,6 +23,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -22,6 +35,7 @@ import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.TimeZone;
+
 import ru.androidlearning.weathertesttask.R;
 import ru.androidlearning.weathertesttask.databinding.MainFragmentBinding;
 import ru.androidlearning.weathertesttask.model.DataLoadState;
@@ -30,6 +44,7 @@ import ru.androidlearning.weathertesttask.model.web.data_sources.api.WeatherDTO;
 public class MainFragment extends Fragment {
     private MainViewModel mainViewModel;
     private MainFragmentBinding binding;
+    private LocationManager locationManager;
 
     private final View.OnClickListener onEnterCityNameClickListener = new View.OnClickListener() {
         @Override
@@ -39,6 +54,64 @@ public class MainFragment extends Fragment {
                 hideKeyboard();
                 mainViewModel.getWeatherByCityNameFromServer(cityName, getString(R.string.units_df_measure), getString(R.string.language));
             }
+        }
+    };
+
+    private final AdapterView.OnItemSelectedListener onChoseSearchTypeListener = new AdapterView.OnItemSelectedListener() {
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            switch (position) {
+                case 0:
+                    binding.enterCityNameLayout.setVisibility(View.VISIBLE);
+                    binding.getWeatherByCoordinatesButton.setVisibility(View.INVISIBLE);
+                    break;
+                case 1:
+                    binding.enterCityNameLayout.setVisibility(View.INVISIBLE);
+                    binding.getWeatherByCoordinatesButton.setVisibility(View.VISIBLE);
+                    checkPermissionAndFineLocation();
+                    break;
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+        }
+    };
+
+    private final ActivityResultLauncher<String> permissionToFineLocationResult = registerForActivityResult(new ActivityResultContracts.RequestPermission(), result -> {
+        if (result) {
+            getSelfLocation();
+        } else {
+            new AlertDialog.Builder(requireContext())
+                    .setTitle(getString(R.string.access_to_fine_location_text))
+                    .setMessage(getString(R.string.explanation_of_fine_location_permission))
+                    .setNegativeButton(getString(R.string.close_button_text), (dialog, which) -> dialog.dismiss())
+                    .create()
+                    .show();
+        }
+    });
+
+    private final LocationListener onLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+            System.out.printf("\n Coordinates: lat: %f, lon = %f\n", lat, lon);
+            locationManager.removeUpdates(this);
+            mainViewModel.getWeatherByCoordinatesFromServer(getString(R.string.units_df_measure), getString(R.string.language), lat, lon);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(@NonNull String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(@NonNull String provider) {
         }
     };
 
@@ -66,6 +139,12 @@ public class MainFragment extends Fragment {
         mainViewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
         mainViewModel.getWeatherLiveData().observe(getViewLifecycleOwner(), this::renderData);
         binding.enterCityNameLayout.setEndIconOnClickListener(onEnterCityNameClickListener);
+
+        ArrayAdapter<?> adapter = ArrayAdapter.createFromResource(requireContext(), R.array.get_weather_types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        binding.choseSearchTypeSpinner.setAdapter(adapter);
+        binding.choseSearchTypeSpinner.setOnItemSelectedListener(onChoseSearchTypeListener);
+        binding.getWeatherByCoordinatesButton.setOnClickListener(v -> checkPermissionAndFineLocation());
 
     }
 
@@ -121,5 +200,41 @@ public class MainFragment extends Fragment {
     private void hideKeyboard() {
         InputMethodManager inputMethodManager = (InputMethodManager) requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(binding.enterCityNameLayout.getWindowToken(), 0);
+    }
+
+    private void checkPermissionAndFineLocation() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getSelfLocation();
+        } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            requestAccessToFineLocationWithDialog();
+        } else {
+            permissionToFineLocationResult.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+    }
+
+    private void requestAccessToFineLocationWithDialog() {
+        new AlertDialog.Builder(requireContext())
+                .setTitle(getString(R.string.access_to_fine_location_title))
+                .setMessage(getString(R.string.explanation_of_fine_location_permission))
+                .setPositiveButton(getString(R.string.grant_access_button_text), (dialog, which) ->
+                        permissionToFineLocationResult.launch(Manifest.permission.ACCESS_FINE_LOCATION))
+                .setNegativeButton(getString(R.string.negative_button_text), (dialog, which) -> dialog.dismiss())
+                .create()
+                .show();
+    }
+
+    private void getSelfLocation() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager = (LocationManager) requireContext().getSystemService(Context.LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+
+            String provider = locationManager.getBestProvider(criteria, true);
+            if (provider != null) {
+                locationManager.requestLocationUpdates(provider, 0, 0, onLocationListener);
+            }
+        } else {
+            requestAccessToFineLocationWithDialog();
+        }
     }
 }
